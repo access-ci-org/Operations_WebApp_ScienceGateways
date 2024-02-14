a29b0724a6e4c8b81f8367f9585ef4df92e01553
Dinuka De Silva <l.dinukadesilva@gmail.com>
2024-02-14 15:28:27 -0500

    Fix the pagination

--- src/ScienceGatewaysList.js
+++ src/ScienceGatewaysList.js
@@ -7,8 +7,6 @@ export default function () {
     let [page, setPage] = useState(1);
     let [pageSize, setPageSize] = useState(20);
     let [count, setCount] = useState(null);
-    let [prev, setPrev] = useState(null);
-    let [next, setNext] = useState(null);
 
     async function fetchData() {
         let query = [];
@@ -32,8 +30,6 @@ export default function () {
 
         console.log("response : ", response);
         setCount(response.count);
-        setPrev(response.prev);
-        setNext(response.next);
         setData(response.results);
     }
 
@@ -44,8 +40,7 @@ export default function () {
 
     function handleSearchKeyDown(e) {
         if (e.key == "Enter") {
-            console.log("handleSearchKeyDown Enter");
-            // fetchData();
+            setPage(1);
             setSearch(e.target.value);
         }
     }
@@ -70,7 +65,7 @@ export default function () {
 
         paginationData.push({
             label: "Next",
-            page: page - 1,
+            page: page + 1,
             disabled: page === lastPage
         });
 
@@ -82,23 +77,47 @@ export default function () {
         setPage(page);
     }
 
+    function handlePagesizeChange(e) {
+        setPageSize(e.target.value);
+    }
+
     useEffect(() => {
         fetchData();
     }, [search, page, pageSize]);
 
     function getGatewayList() {
         if (!data) {
-            return <div className="p-3 text-center">Loading...</div>
+            return <div className="w-100 p-3 text-center">Loading...</div>
         } else if (data.length === 0) {
-            return <div className="p-3 text-center">No science gateways available to display.</div>
+            return <div className="w-100 p-3 text-center">No science gateways available to display.</div>
         } else {
             return <ul className="row list-unstyled">
+                <div className="d-flex flex-row">
+                    <div style={{minWidth: "40px"}} className="d-flex flex-row">
+                        <div className="p-1">
+                            <label className="form-label" htmlFor="pageSizeDropdown">
+                                Page size
+                            </label>
+                        </div>
+                        <div>
+                            <select className="form-control" id="pageSizeDropdown" value={pageSize}
+                                    onChange={handlePagesizeChange.bind(this)}  style={{width: "50px"}}>
+                                <option>10</option>
+                                <option>20</option>
+                                <option>50</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="ps-2 pe-3 pt-2 pb-2 text-end flex-fill">
+                        Showing {data.length} result(s) of {count}.
+                    </div>
+                </div>
                 {data.map((gateway, gatewayIndex) => {
-                    return <div className="m-2 w-100" key={gatewayIndex}>
-                        <div className="bg-light text-dark p-5 h-100">
+                    return <div className="w-100 p-3" key={gatewayIndex}>
+                        <div className="bg-light text-dark p-5 w-100">
                             <div className="d-lg-flex flex-lg-row d-sm-flex flex-sm-column">
                                 <div className="flex-fill">
-                                    <h2>
+                                    <h2 className="w-100">
                                         <a className="btn btn-link">
                                             {gateway.resource_descriptive_name}
                                         </a>
@@ -110,7 +129,7 @@ export default function () {
                             </div>
 
                             <p>{gateway.resource_description}</p>
-                            {/*<p>{gateway.long_description}</p>*/}
+                            {/*/!*<p>{gateway.long_description}</p>*!/*/}
                             <div className="w-100">
                                 <div className="p-1">
                                     <strong>Status : </strong>
@@ -139,22 +158,26 @@ export default function () {
                         </div>
                     </div>
                 })}
-                <nav className="w-100">
-                  <ul className="pagination">
-                      {getPaginationData().map(({page, label, active, disabled}, paginationLinkIndex) => {
-                          return <li className="page-item" key={paginationLinkIndex}>
-                              <a className={"page-link" + (active ? " active" : "") + (disabled ? " disabled" : "")} href="#"
-                                 onClick={handlePaginationLinkClick.bind(this, page)}>{label}</a>
-                          </li>
-                      })}
-                  </ul>
-                </nav>
+                <div className="w-100 d-flex flex-row">
+                    <nav className="flex-fill">
+                        <ul className="pagination">
+                            {getPaginationData().map(({page, label, active, disabled}, paginationLinkIndex) => {
+                                return <li className="page-item" key={paginationLinkIndex}>
+                                    <a className={"page-link" + (active ? " active" : "") + (disabled ? " disabled" : "")}
+                                       href="#"
+                                       onClick={handlePaginationLinkClick.bind(this, page)}>{label}</a>
+                                </li>
+                            })}
+                        </ul>
+                    </nav>
+                </div>
+
             </ul>
         }
     }
 
     return <div className="w-100 p-2 ">
-        <h2>Science Gateway Discovery Interface</h2>
+        <h2 className="w-100">Science Gateway Discovery Interface</h2>
         <div className="w-100 p-3">
             <input type="text" className="form-control" placeholder="Search" onChange={handleSearchChange.bind(this)}
                    onKeyDown={handleSearchKeyDown.bind(this)}/>
