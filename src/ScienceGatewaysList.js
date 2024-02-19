ec3b64480a5d5a7c3d9642eb5abacbfcc1279195
Dinuka De Silva <l.dinukadesilva@gmail.com>
2024-02-19 14:31:23 -0500

    Updated

--- src/ScienceGatewaysList.js
+++ src/ScienceGatewaysList.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import boxArrowUpRightIcon from "bootstrap-icons/icons/box-arrow-up-right.svg";
 
 export default function () {
     let [data, setData] = useState(null);
@@ -149,13 +148,19 @@ export default function () {
                                         <a className="btn btn-link pb-3" href={gateway.primary_service_url}
                                            target="_blank">
                                             Go to gateway &nbsp;&nbsp;
-                                            <img src={boxArrowUpRightIcon}/>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+</svg>
                                         </a>
                                     </li>
                                     <li className="d-inline p-2">
                                         <a className="btn btn-link pb-3" href={gateway.cider_data_url} target="_blank">
                                             Gateway metadata &nbsp;&nbsp;
-                                            <img src={boxArrowUpRightIcon}/>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+</svg>
                                         </a>
                                     </li>
                                 </ul>
